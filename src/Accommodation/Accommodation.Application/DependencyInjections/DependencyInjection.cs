7a7bf44d0ebfc6c9690a373d52b2ce94452320a8
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:29:42 +0500

    created dependency injection class

---
+++ src/Accommodation/Accommodation.Application/DependencyInjections/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace Accommodation.Application.DependencyInjections;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        return services;
+    }
+}
+ 
