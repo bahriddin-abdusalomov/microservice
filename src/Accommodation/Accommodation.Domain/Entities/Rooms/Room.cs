3a9b88c0efc21e17c53a861e93fe194fed6a61a9
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:35:59 +0500

    created Room model

---
+++ src/Accommodation/Accommodation.Domain/Entities/Rooms/Room.cs
@@ -0,0 +1,22 @@
+﻿using Accommodation.Domain.Entities.Bookings;
+using Accommodation.Domain.Entities.Hotels;
+using Accommodation.Domain.Enums.Status;
+
+namespace Accommodation.Domain.Entities.Rooms;
+
+public class Room : BaseEntity
+{
+    public int RoomNumber { get; set; }
+    public RoomStatus Status { get; set; }
+
+    #region relations
+
+    public long HotelId { get; set; }
+    public Hotel Hotel { get; set; }
+
+    public long RoomTypeId { get; set; }
+    public RoomType RoomType { get; set; }
+
+    public Booking Booking { get; set; }
+    #endregion
+}
