9cfd6dd145df3080497dd8ad16bc7ed0546503db
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:30:50 +0500

    add CreateRoomCommandHandler class

---
+++ src/Accommodation/Accommodation.Application/UseCases/Rooms/Handlers/CreateRoomCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Accommodation.Application.UseCases.Rooms.Handlers;
+
+public class CreateRoomCommandHandler : AsyncRequestHandler<CreateRoomCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateRoomCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    protected override async Task Handle(CreateRoomCommand request, CancellationToken cancellationToken)
+    {
+        Room room = new Room()
+        {
+            RoomNumber = request.RoomNumber,
+            Status = request.Status,
+            RoomTypeId = request.RoomTypeId,
+            HotelId = request.HotelId,
+        };
+
+        await _context.Rooms.AddAsync(room);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
