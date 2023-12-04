6fe2f15d7724603dd75231f2004da231676fa6a2
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:36:53 +0500

    created Base Entity model

---
+++ src/Accommodation/Accommodation.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Accommodation.Domain.Entities;
+
+public class BaseEntity
+{
+    public long Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+}
