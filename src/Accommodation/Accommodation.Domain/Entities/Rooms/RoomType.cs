06e08bedb6b1b3a7173f8d63fba357a5495f6f31
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:36:16 +0500

    created RoomType model

---
+++ src/Accommodation/Accommodation.Domain/Entities/Rooms/RoomType.cs
@@ -0,0 +1,17 @@
+﻿using Accommodation.Domain.Entities.Staffs;
+
+namespace Accommodation.Domain.Entities.Rooms;
+
+public class RoomType : BaseEntity  
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    #region relations
+
+    public long RoomId { get; set; }
+    public ICollection<Room> Rooms { get; set; }
+
+    #endregion
+}
