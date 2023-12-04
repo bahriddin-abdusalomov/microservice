4b246c2ea97e77d85cf27a3e020a21cb184e0a20
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:31:36 +0500

    created GLOBAL USING class📃

---
+++ src/Accommodation/Accommodation.Application/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using Accommodation.Domain.Entities.Rooms;
+global using Microsoft.EntityFrameworkCore;
+global using MediatR;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Reflection;
+global using Accommodation.Domain.Enums.Status;
+global using Accommodation.Application.Abstractions;
+global using Accommodation.Application.UseCases.Rooms.Commands;
