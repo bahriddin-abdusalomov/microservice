8a43b99161e02b813b6f273726a69f5214f14db4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:35:39 +0500

    created Hotel model

---
+++ src/Accommodation/Accommodation.Domain/Entities/Hotels/Hotel.cs
@@ -0,0 +1,25 @@
+﻿using Accommodation.Domain.Entities.Rooms;
+using Accommodation.Domain.Entities.Staffs;
+
+namespace Accommodation.Domain.Entities.Hotels;
+
+public class Hotel : BaseEntity
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public int Stars { get; set; }
+    public string ImagePath { get; set; }
+
+    #region relations
+
+    public long StaffId { get; set; }
+    public ICollection<Staff> Staffs { get; set; }
+
+    public long RoomId { get; set; }
+    public ICollection<Room> Rooms { get; set; }
+
+    #endregion
+
+}
