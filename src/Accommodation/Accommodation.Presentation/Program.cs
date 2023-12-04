f89e7fee58a4a18d09226e3809beac26b8d6f22d
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:45:49 +0500

    updated program class

--- src/Accommodation/Accommodation.Presentation/Program.cs
+++ src/Accommodation/Accommodation.Presentation/Program.cs
@@ -1,6 +1,12 @@
+using Accommodation.Application.DependencyInjections;
+using Accommodation.Infrastructure.DependencyInjections;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
