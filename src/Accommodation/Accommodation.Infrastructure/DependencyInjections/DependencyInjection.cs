3aca066dc51039f2d19c074959d7168277eeb771
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:39:24 +0500

    created dependency injection class

---
+++ src/Accommodation/Accommodation.Infrastructure/DependencyInjections/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Accommodation.Application.Abstractions;
+using Accommodation.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Accommodation.Infrastructure.DependencyInjections;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+                   options.UseSqlServer(configuration.GetConnectionString("Connect")));
+
+        return services;
+    }
+}
