eb8c66db7ce3b68abf0d65f192a5132bfd8ca20d
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:36:40 +0500

    created Staff model

---
+++ src/Accommodation/Accommodation.Domain/Entities/Staffs/Staff.cs
@@ -0,0 +1,24 @@
+﻿using Accommodation.Domain.Entities.Hotels;
+using Accommodation.Domain.Enums.Positions;
+
+namespace Accommodation.Domain.Entities.Staffs;
+
+public class Staff : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Position Position { get; set; }
+    public decimal Salary { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public DateTime HireDate { get; set; }
+    public string ImagePath { get ; set; }
+    
+    #region relations
+
+    public long HotelId { get; set; }
+    public  Hotel Hotel { get; set; }
+
+    #endregion
+}
