b2b42a125c8f0d61aec83bfa7128766cb6ec5f80
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:45:33 +0500

    created Room controller

---
+++ src/Accommodation/Accommodation.Presentation/Controllers/RoomsController.cs
@@ -0,0 +1,24 @@
+﻿using Accommodation.Application.UseCases.Rooms.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accommodation.Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoomsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public RoomsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateAsync(CreateRoomCommand command)
+    {
+        await _mediator.Send(command);
+        return Ok();
+    }
+}
