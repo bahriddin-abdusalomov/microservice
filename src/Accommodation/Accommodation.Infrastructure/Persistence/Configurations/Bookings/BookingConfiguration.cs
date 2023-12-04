dab656bf913bed02797272180b6da03bddaa584c
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:41:01 +0500

    created Booking Configuration class

---
+++ src/Accommodation/Accommodation.Infrastructure/Persistence/Configurations/Bookings/BookingConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Accommodation.Domain.Entities.Bookings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accommodation.Infrastructure.Persistence.Configurations.Bookings;
+
+public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder.HasOne(r => r.Guest)
+            .WithMany(h => h.Bookings)
+            .HasForeignKey(r => r.GuestId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(r => r.Room)
+            .WithOne(b => b.Booking)
+            .HasForeignKey<Booking>(b => b.RoomId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
