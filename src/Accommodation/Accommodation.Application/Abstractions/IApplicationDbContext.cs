f521b263e757f00dbf1b86268026ea0548226820
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:29:13 +0500

    created IApplicationDbContext interface🫚

---
+++ src/Accommodation/Accommodation.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Accommodation.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Room> Rooms { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken collactionToken = default);
+}       
