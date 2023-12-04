f2ed44df5c38826f1f04c4a18100a33fdb127917
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:37:28 +0500

    created Position enum

---
+++ src/Accommodation/Accommodation.Domain/Enums/Positions/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace Accommodation.Domain.Enums.Positions;
+
+public enum Position
+{
+    AssistantHotelManager = 1,
+    HotelHanager,
+    DirectorOfPurchasing,
+    DirectorOfSalesndMarketing,
+    RoomAttendant,
+    NightAuditor,
+    ParkingAttendant,
+    HotelReceptionist
+}
