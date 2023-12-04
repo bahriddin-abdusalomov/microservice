5ae73fe1b907423ca4a94d5b4b48b59e1e7e6026
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:43:44 +0500

    created Room Type Configuration class

---
+++ src/Accommodation/Accommodation.Infrastructure/Persistence/Configurations/Rooms/RoomTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Accommodation.Domain.Entities.Rooms;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accommodation.Infrastructure.Persistence.Configurations.Rooms;
+
+public class RoomTypeConfiguration : IEntityTypeConfiguration<RoomType>
+{
+    public void Configure(EntityTypeBuilder<RoomType> builder)
+    {
+        builder.HasKey(rt => rt.Id);
+
+        builder.Property(rt => rt.Name)
+            .IsRequired()
+            .HasMaxLength(20); 
+
+        builder.Property(rt => rt.Price)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.HasMany(rt => rt.Rooms)
+            .WithOne(r => r.RoomType)
+            .HasForeignKey(r => r.RoomTypeId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
