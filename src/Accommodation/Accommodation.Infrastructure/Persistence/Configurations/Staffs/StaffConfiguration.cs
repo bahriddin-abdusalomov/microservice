d72727e857e190906c74a3159b570ec890895457
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:43:54 +0500

    created Staff Configuration class

---
+++ src/Accommodation/Accommodation.Infrastructure/Persistence/Configurations/Staffs/StaffConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Accommodation.Domain.Entities.Staffs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accommodation.Infrastructure.Persistence.Configurations.Staffs;
+
+public class StaffConfiguration : IEntityTypeConfiguration<Staff>
+{
+    public void Configure(EntityTypeBuilder<Staff> builder)
+    {
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(s => s.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(s => s.Salary)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(s => s.Phone)
+            .HasDefaultValue("+998944396669")
+            .IsRequired()
+            .HasMaxLength(13);
+
+        builder.Property(s => s.Email)
+            .HasDefaultValue("example@gmail.com")
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasOne(s => s.Hotel)
+            .WithMany(h => h.Staffs)
+            .HasForeignKey(s => s.HotelId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.ToTable("Staffs");
+    }
+}
