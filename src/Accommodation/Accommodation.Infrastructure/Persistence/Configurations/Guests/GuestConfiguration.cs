a80d479cc76c57fd071285b6dea36fc23eb034a9
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:41:36 +0500

    created Guest Configuration class

---
+++ src/Accommodation/Accommodation.Infrastructure/Persistence/Configurations/Guests/GuestConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Accommodation.Domain.Entities.Guests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accommodation.Infrastructure.Persistence.Configurations.Guests;
+
+public class GuestConfiguration : IEntityTypeConfiguration<Guest>
+{
+    public void Configure(EntityTypeBuilder<Guest> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder.Property(s => s.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(s => s.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(s => s.Phone)
+            .HasDefaultValue("+998944396669")
+            .IsRequired()
+            .HasMaxLength(13);
+
+        builder.Property(s => s.Email)
+            .HasDefaultValue("example@gmail.com")
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
