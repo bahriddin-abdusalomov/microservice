62a7a006aada7e41134298ccf0dba7e328e46ae1
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:43:31 +0500

    created Room Configuration class

---
+++ src/Accommodation/Accommodation.Infrastructure/Persistence/Configurations/Rooms/RoomConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Accommodation.Domain.Entities.Bookings;
+using Accommodation.Domain.Entities.Rooms;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accommodation.Infrastructure.Persistence.Configurations.Rooms;
+
+public class RoomConfiguration : IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder.Property(r => r.RoomNumber)
+            .IsRequired();
+
+        builder.Property(r => r.Status)
+            .IsRequired();
+
+        builder.HasOne(r => r.Hotel)
+            .WithMany(h => h.Rooms)
+            .HasForeignKey(r => r.HotelId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(r => r.RoomType)
+            .WithMany(rt => rt.Rooms)
+            .HasForeignKey(r => r.RoomTypeId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(r => r.Booking)
+            .WithOne(b => b.Room)
+            .HasForeignKey<Booking>(b => b.RoomId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.ToTable("Rooms");
+    }
+}
