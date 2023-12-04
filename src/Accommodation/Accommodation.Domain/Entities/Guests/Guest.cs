8e71b6a4e0eba5546cf741fbfd53cae59e84fcc1
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-04 16:35:28 +0500

    created Guest model

---
+++ src/Accommodation/Accommodation.Domain/Entities/Guests/Guest.cs
@@ -0,0 +1,23 @@
+﻿using Accommodation.Domain.Entities.Bookings;
+using Accommodation.Domain.Entities.Rooms;
+using Accommodation.Domain.Entities.Staffs;
+
+namespace Accommodation.Domain.Entities.Guests;
+
+public class Guest : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string ImagePath { get; set; }
+
+    #region relations
+
+    public long BookingId { get; set; }
+    public ICollection<Booking> Bookings { get; set; }
+
+    #endregion
+}
