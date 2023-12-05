7858d88d7a9ee3cb9ae8eddc6b5176e412a5d4e7
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:47:00 +0500

    created ImageNotFoundException🔴

---
+++ src/Accommodation/Accommodation.Domain/Exceptions/Images/ImageNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accommodation.Domain.Exceptions.Images;
+
+public class ImageNotFoundException : NotFoundException
+{
+    public ImageNotFoundException()
+    {
+        Message = "Image not found !";
+    }
+}
