c5d46e84bba87fbcaf3563ed8d14a688a53355e4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:26:31 +0500

    add DeleteHotelCommandHandler class🏚️

---
+++ src/Accommodation/Accommodation.Application/UseCases/Hotels/Handlers/DeleteHotelCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Accommodation.Application.UseCases.Hotels.Handlers;
+
+public class DeleteHotelCommandHandler : IRequestHandler<DeleteHotelCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IFileService _fileService;
+
+    public DeleteHotelCommandHandler(IApplicationDbContext context, IFileService fileService)
+    {
+        _context = context;
+        _fileService = fileService;
+    }
+
+    public async Task<int> Handle(DeleteHotelCommand request, CancellationToken cancellationToken)
+    {
+        Hotel hotel = await _context.Hotels.FirstAsync(hotel => hotel.Id == request.Id);
+        if (hotel == null) throw new HotelNotFoundException();
+
+        bool image = await _fileService.DeleteImageAsync(hotel.ImagePath);
+        if (image == false) throw new ImageNotFoundException();
+
+        _context.Hotels.Remove(hotel);
+        int resutlt = await _context.SaveChangesAsync(cancellationToken);
+
+        return resutlt;
+    }                                                 
+}
