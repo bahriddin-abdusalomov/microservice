28d6a11726739cb73aae51e6e716df4bf58f63a1
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:41:42 +0500

    add GetAllRoomQueryHandler class

---
+++ src/Accommodation/Accommodation.Application/UseCases/Rooms/Handlers/GetAllRoomQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Accommodation.Application.UseCases.Rooms.Handlers;
+public class GetAllRoomQueryHandler : IRequestHandler<GetAllRoomQuery, List<Room>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetAllRoomQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Room>> Handle(GetAllRoomQuery request, CancellationToken cancellationToken)
+    {
+        var rooms = await _context.Rooms.ToListAsync();
+        return rooms;
+    }
+}
