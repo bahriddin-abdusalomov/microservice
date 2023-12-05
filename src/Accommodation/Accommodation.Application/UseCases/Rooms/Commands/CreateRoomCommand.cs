7fff57a084e17416446eb3945f0305954fa67321
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:39:54 +0500

    add CreatedRoomCommand class 🍱

--- src/Accommodation/Accommodation.Application/UseCases/Rooms/Commands/CreateRoomCommand.cs
+++ src/Accommodation/Accommodation.Application/UseCases/Rooms/Commands/CreateRoomCommand.cs
@@ -1,10 +1,9 @@
 ﻿namespace Accommodation.Application.UseCases.Rooms.Commands;
 
-public class CreateRoomCommand : IRequest
+public class CreateRoomCommand : IRequest<int>
 {
     public int RoomNumber { get; set; }
     public RoomStatus Status { get; set; }
     public long HotelId { get; set; }
     public long RoomTypeId { get; set; }
-
 }
