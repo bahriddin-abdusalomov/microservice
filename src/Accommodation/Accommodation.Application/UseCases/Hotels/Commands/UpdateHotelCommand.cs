4bb5bf142a1c1894facccdc12ca8568c97f46f8c
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:22:31 +0500

    add UpdateHotelCommand class 🏡

---
+++ src/Accommodation/Accommodation.Application/UseCases/Hotels/Commands/UpdateHotelCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Accommodation.Application.UseCases.Hotels.Commands;
+
+public class UpdateHotelCommand : IRequest<int>
+{
+    public long HotelId { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public int Stars { get; set; }
+    public IFormFile ImagePath { get; set; }
+    public long StaffId { get; set; }
+    public long RoomId { get; set; }
+}
