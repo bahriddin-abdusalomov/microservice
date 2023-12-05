c8ee84e2eb1cd0e8ea546e526c0f2b0e10a6b392
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:09:18 +0500

    created IApplication class➕

--- src/Accommodation/Accommodation.Application/Abstractions/IApplicationDbContext.cs
+++ src/Accommodation/Accommodation.Application/Abstractions/IApplicationDbContext.cs
@@ -3,6 +3,11 @@
 public interface IApplicationDbContext
 {
     public DbSet<Room> Rooms { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<RoomType> RoomTypes { get; set; }
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<Staff> Staffs { get; set; }
 
     public Task<int> SaveChangesAsync(CancellationToken collactionToken = default);
 }       
