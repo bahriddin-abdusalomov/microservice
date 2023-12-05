206e6879ec4360a830f79892ca507af6270760be
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:19:10 +0500

    created MediaHelper class🖼️

---
+++ src/Accommodation/Accommodation.Application/Services/Helpers/MediaHelpers.cs
@@ -0,0 +1,31 @@
+﻿namespace Accommodation.Application.Services.Helpers;
+
+public class MediaHelper
+{
+    public static string MakeImageName(string filename)
+    {
+        FileInfo fileInfo = new FileInfo(filename);
+
+        string[] ImageExtension = GetImageExtensions();
+
+        if (ImageExtension.Any(x => x == fileInfo.Extension))
+        {
+            string extension = fileInfo.Extension;
+            string name = "IMG_" + Guid.NewGuid() + extension;
+            return name;
+        }
+        return string.Empty;
+    }
+
+    public static string[] GetImageExtensions()
+    {
+        return new string[]
+        {
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".bmp",
+            ".svg"
+        };
+    }
+}
