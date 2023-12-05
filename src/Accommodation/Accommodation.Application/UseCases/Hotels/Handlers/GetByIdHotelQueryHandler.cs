174fb16a5596c98a99bef95d950349bdcdea19f8
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:27:51 +0500

    add GetByIdHotelQueryHandler class🏡

---
+++ src/Accommodation/Accommodation.Application/UseCases/Hotels/Handlers/GetByIdHotelQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Accommodation.Application.UseCases.Hotels.Handlers;
+
+public class GetByIdHotelQueryHandler : IRequestHandler<GetByIdHotelQuery, Hotel>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetByIdHotelQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Hotel> Handle(GetByIdHotelQuery request, CancellationToken cancellationToken)
+    {
+        var hotel = await _context.Hotels.FindAsync(request.Id);
+        if (hotel == null) throw new HotelNotFoundException();
+
+        return hotel;
+    }
+}
