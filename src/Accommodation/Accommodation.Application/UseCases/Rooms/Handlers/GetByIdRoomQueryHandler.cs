a081131e33df805086f58e7d31e6895262ded149
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:42:18 +0500

    add GetByIdRoomQueryHandler class

---
+++ src/Accommodation/Accommodation.Application/UseCases/Rooms/Handlers/GetByIdRoomQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Accommodation.Application.UseCases.Rooms.Queries;
+using Accommodation.Domain.Exceptions.Rooms;
+
+namespace Accommodation.Application.UseCases.Rooms.Handlers;
+public class GetByIdRoomQueryHandler : IRequestHandler<GetByIdRoomQuery, Room>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetByIdRoomQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Room> Handle(GetByIdRoomQuery request, CancellationToken cancellationToken)
+    {
+        var room = await _context.Rooms.FindAsync(request.Id);
+        if (room == null) throw new RoomNotFoundException();
+
+        return room;
+    }
+}
