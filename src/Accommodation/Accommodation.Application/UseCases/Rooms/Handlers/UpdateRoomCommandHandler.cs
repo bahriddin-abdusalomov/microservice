979e7a58431031823a608c56fc001188f5c9b04d
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:41:56 +0500

    add UpdateRoomCommandHandler class

---
+++ src/Accommodation/Accommodation.Application/UseCases/Rooms/Handlers/UpdateRoomCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace Accommodation.Application.UseCases.Rooms.Handlers;
+
+
+public class UpdateRoomCommandHandler : IRequestHandler<UpdateRoomCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public UpdateRoomCommandHandler(IApplicationDbContext context, IFileService fileService)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(UpdateRoomCommand request, CancellationToken cancellationToken)
+    {
+        Room? room = await _context.Rooms.FirstOrDefaultAsync(room => room.Id == request.Id);
+        if (room == null) throw new RoomNotFoundException();
+
+
+        room.RoomNumber = request.RoomNumber;
+        room.Status = request.Status;
+        room.RoomTypeId = request.RoomTypeId;
+        room.HotelId = request.HotelId;
+        room.CreatedDate = DateTime.UtcNow;
+        room.UpdatedDate = DateTime.UtcNow;
+
+        _context.Rooms.Update(room);
+        int result = await _context.SaveChangesAsync();
+        return result;
+    }
+}
+
