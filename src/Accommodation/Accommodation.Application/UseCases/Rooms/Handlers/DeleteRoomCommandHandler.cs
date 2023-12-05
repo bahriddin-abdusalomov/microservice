e2d56e911b56f199f38933dc98e2cd9ec7d925a2
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:41:16 +0500

    add DeleteRoomCommandHandler class

---
+++ src/Accommodation/Accommodation.Application/UseCases/Rooms/Handlers/DeleteRoomCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Accommodation.Application.UseCases.Rooms.Handlers;
+
+public class DeleteRoomCommandHandler : IRequestHandler<DeleteRoomCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteRoomCommandHandler(IApplicationDbContext context, IFileService fileService)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(DeleteRoomCommand request, CancellationToken cancellationToken)
+    {
+        Room room = await _context.Rooms.FirstAsync(room => room.Id == request.Id);
+        if (room == null) throw new RoomNotFoundException();
+
+        _context.Rooms.Remove(room);
+        int resutlt = await _context.SaveChangesAsync(cancellationToken);
+
+        return resutlt;
+    }
+}
