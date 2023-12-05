6797b10d5d45cd39975cb85d06d8bee91270b948
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:49:17 +0500

    created RoomsController

--- src/Accommodation/Accommodation.Presentation/Controllers/RoomsController.cs
+++ src/Accommodation/Accommodation.Presentation/Controllers/RoomsController.cs
@@ -1,10 +1,12 @@
 ﻿using Accommodation.Application.UseCases.Rooms.Commands;
+using Accommodation.Application.UseCases.Rooms.Queries;
 using MediatR;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace Accommodation.Presentation.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 [ApiController]
 public class RoomsController : ControllerBase
 {
@@ -16,9 +18,41 @@ public class RoomsController : ControllerBase
     }
 
     [HttpPost]
-    public async ValueTask<IActionResult> CreateAsync(CreateRoomCommand command)
+    public async ValueTask<IActionResult> CreateRoomAsync([FromForm] CreateRoomCommand command)
     {
-        await _mediator.Send(command);
-        return Ok();
+        int result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetByIdRoomAsync(long id)
+    {
+        var command = new GetByIdRoomQuery { Id = id };
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllRoomAsync()
+    {
+        var result = await _mediator.Send(new GetAllRoomQuery());
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateRoomAsync([FromForm] UpdateRoomCommand command)
+    {
+        int result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async ValueTask<IActionResult> DeleteRoomAsync(long id)
+    {
+        var command = new DeleteRoomCommand { Id = id };
+
+        int result = await _mediator.Send(command);
+        return Ok(result);
     }
 }
