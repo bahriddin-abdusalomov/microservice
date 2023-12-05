19a520f5f73cf7a229cb013e7760a5a2ea88a5bd
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:21:15 +0500

    add CreateHotelCommand  class🛖

---
+++ src/Accommodation/Accommodation.Application/UseCases/Hotels/Commands/CreateHotelCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Accommodation.Application.UseCases.Hotels.Commands;
+
+public class CreateHotelCommand : IRequest<int>
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public int Stars { get; set; }
+    public IFormFile ImagePath { get; set; }
+    public long StaffId { get; set; }
+    public long RoomId { get; set; }
+}
