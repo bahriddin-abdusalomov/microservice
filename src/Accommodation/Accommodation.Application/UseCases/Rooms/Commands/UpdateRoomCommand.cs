b0d1cc4a50819560678a3cdc73fe5bbbf20f6a2a
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-05 14:40:32 +0500

    add UpdateRoomCommand class

---
+++ src/Accommodation/Accommodation.Application/UseCases/Rooms/Commands/UpdateRoomCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Accommodation.Application.UseCases.Rooms.Commands;
+
+public class UpdateRoomCommand : IRequest<int>
+{
+    public int Id { get; set; }
+    public int RoomNumber { get; set; }
+    public RoomStatus Status { get; set; }
+    public long HotelId { get; set; }
+    public long RoomTypeId { get; set; }
+}
