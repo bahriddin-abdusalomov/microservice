91e6d1f069850ecd4d477b99c33aa30c6e8bb5bc
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-06 12:04:19 +0500

    created attribute for property➡️

---
+++ src/Accommodation/Accommodation.Application/Attributes/Email/EmailAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Accommodation.Application.Atributes.Email;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class EmailAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value == null) return false;
+
+        string? email = value.ToString();
+
+        if (System.Text.RegularExpressions.Regex.IsMatch(email, @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$"))
+            return true;
+
+        return false;
+    }
+}
---
+++ src/Accommodation/Accommodation.Application/Attributes/Phone/PhoneAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Accommodation.Application.Attributes.Phone;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class PhoneNumberAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value == null) return false;
+
+        string? phoneNumber = value.ToString();
+
+        if (System.Text.RegularExpressions.Regex.IsMatch(phoneNumber, @"^\+\d{12}$"))
+            return true;
+
+        return false;
+    }
+}
